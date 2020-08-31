bf42a18e7819f54cb0ec89c74587e6e3f48a6350
YJHoon <yjhoon0816@gmail.com>
2020-08-31 23:04:05 +0900

    Post랑 Comment 만듦. 여기는 AbstractUser로 User 설정

--- django_sns/settings.py
+++ django_sns/settings.py
@@ -126,7 +126,7 @@ USE_TZ = False
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
-    'allauth.account_auth_backends.AuthenticationBackend'
+    'allauth.account.auth_backends.AuthenticationBackend'
 ]
 
 SITE_ID = 1
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=50, null=False)
+    content = models.TextField()
+    view_count = models.IntegerField(default=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete = models.CASCADE, related_name='comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
